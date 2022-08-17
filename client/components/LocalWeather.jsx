56c15252e44b1373cdce036d0a0b490c12d5d854
Vance M Mcgrady <68658084+VanceMcGrady@users.noreply.github.com>
2022-08-17 11:28:55 -0700

    successfully retreiving coordinate data and creating valid url based on lat/long

--- client/components/LocalWeather.jsx
+++ client/components/LocalWeather.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { FunnelChart } from "recharts";
 
 
 
 const LocalWeather = props => {
 
+const getWeather = () => {
+
+    let api = "https://api.openweathermap.org/data/2.5/weather";
+    let apiKey = "3865d44fbc4bfd45572787afb8fa06b2"; 
+   
+    
+    navigator.geolocation.getCurrentPosition(success, error); 
+    
+    function success(position){
+        let latitude = position.coords.latitude; 
+        let longitude = position.coords.longitude; 
+        let url =  `${api}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`; 
+        
+        console.log('position ', position);
+        console.log('latitude: ', latitude);
+        console.log('longitude: ', longitude); 
+        console.log('url: ', url)
+    }; 
+
+    function error(){console.log('error finding location')}
+
+
+
+}
+
+getWeather()
 
     return(
     <div className="localWeather">
