67c39557e0d0ce683c14c3823bce8a423c37ffb6
Hannah McDowell <hannah.mcdowell1@gmail.com>
2022-08-17 22:43:10 -0700

    Refactor to pass down state variable indicating if a user has current location data.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -15,51 +15,45 @@ const App = props => {
   const [user, setUser] = useState(null);
   const [localInfo, setLocalInfo] = useState({});
   const [currInfoLoading, setCurrInfoLoading] = useState(true);
+  const [locationToLoad, setLocationToLoad] = useState(false);
 
 
   useEffect(()=>{
-
-
     const currentLocationInfo = (() => {
-     
-  
-       let api = "https://api.openweathermap.org/data/2.5/weather";
-       let apiKey = "3865d44fbc4bfd45572787afb8fa06b2"; 
-     
+      let api = "https://api.openweathermap.org/data/2.5/weather";
+      let apiKey = "3865d44fbc4bfd45572787afb8fa06b2"; 
+
       navigator.geolocation.getCurrentPosition(success, error); 
       
       function success(position){
-          let latitude = position.coords.latitude; 
-          let longitude = position.coords.longitude; 
-          let url =  `${api}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`; 
-          
-          let localInfo = {}
-  
-          fetch(url)
-          .then(res => res.json())
-          .then(data => {           
-             localInfo.temp = data.main.temp;
-             localInfo.currentLocation = data.name
-             localInfo.weatherDescription = data.weather[0].main;
-             localInfo.latitude = latitude
-             localInfo.longitude = longitude
+        setLocationToLoad(true);
+        let latitude = position.coords.latitude; 
+        let longitude = position.coords.longitude; 
+        let url =  `${api}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`; 
+        
+        let localInfo = {}
 
-             setLocalInfo(localInfo)
-             setCurrInfoLoading(false);
-          })
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {           
+          localInfo.temp = data.main.temp;
+          localInfo.currentLocation = data.name
+          localInfo.weatherDescription = data.weather[0].main;
+          localInfo.latitude = latitude
+          localInfo.longitude = longitude
+
+          setLocalInfo(localInfo)
+          setCurrInfoLoading(false);
+        })
       }; 
   
       function error() {
         console.log('error finding location');
+        setCurrInfoLoading(false);
       }
     })();  
-
-
   }, [])
 
-  
-
-  
     return (
       <Routes>
         <Route path="/" element={
@@ -67,7 +61,12 @@ const App = props => {
             <Navbar user={user} setUser={setUser} />
             <LocalWeather localInfo = {localInfo}/>
             {!currInfoLoading ?
-              <Searchbar localInfo = {localInfo} user={user} setUser={setUser} /> :
+              <Searchbar 
+                localInfo={localInfo} 
+                locationToLoad={locationToLoad} 
+                setLocationToLoad={setLocationToLoad} 
+                user={user} 
+                setUser={setUser} /> :
               <div><h3>Loading Location Info</h3></div>
             }
           </main>
