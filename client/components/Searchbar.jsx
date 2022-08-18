9d06ff38308c15b48b3d631ae899905bf163d5fb
Hannah McDowell <hannah.mcdowell1@gmail.com>
2022-08-17 22:47:39 -0700

    Render error messages upon no data returned from search, modularize repeated code.

--- client/components/Searchbar.jsx
+++ client/components/Searchbar.jsx
@@ -4,50 +4,64 @@ import axios from 'axios';
 import CardContainer from './CardContainer.jsx';
 import { LineChart } from 'recharts';
 
-
 const Searchbar = props => {
-
-  //[current, updateMethod] = usesState('default') 
   
-  const [cityData, setCityData] = useState([]) // cityData = []
- 
-  const [cityName, setCityName] = useState(props.localInfo.currentLocation) // cityName = 'Portland'
-  const [locationIsLoading, setLocationIsLoading] = useState(true)
+  const [cityData, setCityData] = useState([]);
+  const [cityName, setCityName] = useState('');
+  const [locationIsLoading, setLocationIsLoading] = useState(true);
+  const [error, setError] = useState('');
 
-  //like componentDidMount triggers on change
-  useEffect(() => {
-  
-
+  // function to handle async GET requests to DB
+  const useAsyncData = (city) => {
     axios({
       method: 'POST',
       url: 'http://localhost:3000/search',
-      data: { city: props.localInfo.currentLocation }
+      data: { city }
     })
       .then(data => {
+        if (data.data.cityData.length === 0) {
+          setError(`Weather data not found for ${data.data.cityName}, please enter a different location.`);
+          setLocationIsLoading(false);
+          props.setLocationToLoad(false);
+          return;
+        }
         setCityData(data.data.cityData);
         setCityName(data.data.cityName);
-        setLocationIsLoading(false)
+        setLocationIsLoading(false);
       })
+      .catch(err => {
+        console.log('Error in post request for location data:', err);
+        setError('City not found, please enter a different location.');
+        setLocationIsLoading(false);
+        props.setLocationToLoad(false);
+      })
+  }
+
+
+  //like componentDidMount triggers on change
+  useEffect(() => {
+    if (props.locationToLoad) {
+      const loadData = async () => {
+        await useAsyncData(props.localInfo.currentLocation);
+      }
+      loadData();
+    } else {
+      setLocationIsLoading(false);
+    }
   }, []);
 
   const handleSubmit = (e) => {
     //prevents the page from refreshing on submit
     e.preventDefault();
     if (!locationIsLoading) {
+      setError('');
       setLocationIsLoading(true);
+      props.setLocationToLoad(true);
       const inputCity = e.target.firstChild.value;
-      console.log('input city', inputCity);
-      axios({
-        method: 'POST',
-        url: 'http://localhost:3000/search',
-        data: { city: inputCity }
-      })
-        .then(data => {
-          // console.log('front end!!', data.data);
-          setCityData(data.data.cityData);
-          setCityName(data.data.cityName);
-          setLocationIsLoading(false);
-        })
+      const loadData = async () => {
+        await useAsyncData(inputCity);
+      }
+      loadData();
     }
   }
 
@@ -57,7 +71,16 @@ const Searchbar = props => {
         <input className="search-input" name="search-input" id="search-input" type="text" placeholder="London" required></input>
         <input className="search-btn" type="submit" value="Search"></input>
       </form>
-      {!locationIsLoading ? <CardContainer city={cityName} data={cityData} /> : <div><h3>Enter city name above...</h3></div>}
+      {
+        !locationIsLoading && props.locationToLoad ? 
+        <CardContainer city={cityName} data={cityData} /> : 
+        <div><h3>Enter city name above...</h3></div>
+      }
+      {
+        error ?
+        <div><h3>{error}</h3></div> :
+        null
+      }
     </div>
   )
 }
